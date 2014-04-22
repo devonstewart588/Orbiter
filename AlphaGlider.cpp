e1f85f03fd102833d8a86faec286d29eed4d3d5f
devonstewart588 <Devonstewart588@gmail.com>
2014-04-22 07:30:01 -0500

    Create AlphaGlider.cpp
    For my Alphaglider
    Bbased on Delta Glider

---
+++ AlphaGlider.cpp
@@ -0,0 +1,65 @@
+#define ORBITER_MODULE
+
+#include "Template.h"
+
+void VLiftCoeff (double aoa, double M, double Re, double *cl, double *cm, double *cd)
+{
+}
+
+void HLiftCoeff (double beta, double M, double Re, double *cl, double *cm, double *cd)
+{
+}
+
+Template::Template (OBJHANDLE hObj, int fmodel)
+: VESSEL2 (hObj, fmodel)
+{
+}
+
+
+Template::~Template ()
+{
+}
+
+void Template::clbkSetClassCaps (FILEHANDLE cfg)
+{
+}
+
+void Template::clbkLoadStateEx (FILEHANDLE scn, void *vs)
+{
+}
+
+void Template::clbkSaveState (FILEHANDLE scn)
+{
+}
+
+
+void Template::clbkPreStep (double simt, double simdt, double mjd)
+{
+}
+
+void Template::clbkPostStep (double simt, double simdt, double mjd)
+{
+}
+
+
+int Template::clbkConsumeBufferedKey (DWORD key, bool down, char *kstate)
+{
+}
+
+
+DLLCLBK void InitModule (HINSTANCE hModule)
+{
+}
+
+
+DLLCLBK void ExitModule (HINSTANCE hModule)
+{
+}
+
+DLLCLBK VESSEL *ovcInit (OBJHANDLE hvessel, int flightmodel)
+{	
+}
+
+DLLCLBK void ovcExit (VESSEL *vessel)
+{
+}
