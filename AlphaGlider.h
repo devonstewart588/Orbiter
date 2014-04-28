7e3397e2cb34bc30046ad22c0f7888603fca4fbe
devonstewart588 <Devonstewart588@gmail.com>
2014-04-28 19:54:37 +0100

    Update AlphaGlider.h

--- AlphaGlider.h
+++ AlphaGlider.h
@@ -10,6 +10,10 @@ public:
 	AlphaGlider (OBJHANDLE hObj, int fmodel);
 	~AlphaGlider ();
 	void clbkLoadStateEx (FILEHANDLE scn, void *vs);
+	private: double myparam; }; void MyVessel::clbkLoadStateEx (FILEHANDLE scn, void *status)
+	{ char *line;
+	while (oapiReadScenario_nextline (scn, line)) if (!strnicmp (line, "MYPARAM", 7)) { sscanf (line+7, "%lf", &myparam); } else { ParseScenarioLineEx (line, status); } } }
+	{
 	void clbkSaveState (FILEHANDLE scn);
 	int  clbkConsumeBufferedKey (DWORD key, bool down, char *kstate);
 	void clbkSetClassCaps (FILEHANDLE cfg);
